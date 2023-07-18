e35672e4820bbdb431c1222905ee4a30ced85658
Temitope Oluwaloni <temitope15aug@yahoo.com>
2023-07-18 10:42:48 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
- * @n: print from this number
+ * @n: The number to start printing from
+ * Return: Always 0.
  */
 
 void print_to_98(int n)
